4a1b94d6c7968c69e4bda6e9489fc176c0a780a2
Ankit Pandey <ankitpandey.86040@gmail.com>
2026-06-28 13:01:50 +0530

    Completed Try -Catch in Java

---
+++ MULTIPLEINHERITANCEBYINTERFACE.java
@@ -0,0 +1,26 @@
+ interface Camera{
+    void clickPhoto();
+}
+
+interface MusicPlayer{
+    void PlayMusic();
+}
+
+class Smartphone implements Camera,MusicPlayer {
+    @Override
+    public void clickPhoto() {
+        System.out.println("clicked");
+    }
+
+    @Override
+    public void PlayMusic() {
+        System.out.println("music played ");
+    }
+}
+    public class MULTIPLEINHERITANCEBYINTERFACE{
+    public void main(String[] args) {
+        Smartphone phone = new Smartphone();
+        phone.clickPhoto();
+        phone.PlayMusic();
+    }
+}
